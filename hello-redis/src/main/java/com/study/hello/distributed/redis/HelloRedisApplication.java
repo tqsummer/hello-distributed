d28d0b3fe6d9e28f0e6fc958312062f50e0285b7
fangxiangqian <tqsummer@gmail.com>
2023-10-24 10:18:10 +0800

    add code

---
+++ hello-redis/src/main/java/com/study/hello/distributed/redis/HelloRedisApplication.java
@@ -0,0 +1,15 @@
+package com.study.hello.distributed.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fangxiangqian
+ */
+@SpringBootApplication
+public class HelloRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloRedisApplication.class, args);
+    }
+
+}
